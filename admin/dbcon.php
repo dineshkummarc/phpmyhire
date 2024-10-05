81349f559f6cb11c294a31f0a5277bc5d9be1a22
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-05 22:04:07 +0530

    updated

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $password = '';
-    $database = 'projects';
+    $database = 'phpmyhire';
 
     $con = mysqli_connect($host, $user, $password, $database);
 
